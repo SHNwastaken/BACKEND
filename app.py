9feda396a182986bdec9c9f0ac6a1d99ac5bea5e
SHNwastaken <srhari101@gmail.com>
2024-10-06 22:19:16 +0530

    eleventh commit

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from langchain.chains import create_retrieval_chain
 import google.generativeai as genai
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": ["https://nasa-pace.netlify.app"]}})
+CORS(app, resources={r"/*": {"origins": ["https://nasa-pace-edu.vercel.app"]}})
 
 
 # Load environment variables
